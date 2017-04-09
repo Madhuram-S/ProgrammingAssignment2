3fc61904ca0890da5af4460987234afc7846d3a1
Madhuram S <madhuananth04@icloud.com>
2017-04-09 00:19:58 -0500

    commit file

--- cachematrix.R
+++ cachematrix.R
@@ -6,11 +6,14 @@
 
 makeCacheMatrix <- function(orig_m = matrix()) {
   inv_m <- NULL ## initialize cache matrix to NULL
+  
+  ## set original matrix if a new one is provided, "<<-" ensure the cached matrix is changed.
   set <- function(new_m){
     orig_m <<- new_m
-    inv_m <<- NULL
+    inv_m <<- NULL ## set inverse matrix to NULL for recalculation
   }
   
+  ## get matrix provided
   get <- function() orig_m
   
   setInv <- function(solve) inv_m <<- solve
@@ -26,14 +29,15 @@ cacheSolve <- function(orig_m, ...) {
   ## Return a matrix that is the inverse of original matrix
   inv_m <- orig_m$getInv()
   
+  ## check if a cached value of inverse matrix exist, if yes return the matrix inverse
   if(!is.null(inv_m)){
     message("printing inverse matrix from cache")
     return(inv_m)
   }
   
-  ## solve the inverse of the matrix
-  mx <- orig_m$get()
-  inv_m <- solve(mx)
-  orig_m$setInv(inv_m)
+  ## If inverse matrix does not exist, solve the inverse of the matrix
+  mx <- orig_m$get() 
+  inv_m <- solve(mx) ## using solve function to get inverse of a matrix
+  orig_m$setInv(inv_m) ## set the value to cache
   inv_m
 }
